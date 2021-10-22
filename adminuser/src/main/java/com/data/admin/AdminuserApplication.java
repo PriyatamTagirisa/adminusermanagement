0fb45e0aa3358e40d1ae7aa999cc36678690d52c
Dell <Dell@Dell-PC>
2021-10-22 11:36:27 +0530

    First Commit

---
+++ adminuser/src/main/java/com/data/admin/AdminuserApplication.java
@@ -0,0 +1,13 @@
+package com.data.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminuserApplication.class, args);
+	}
+
+}
